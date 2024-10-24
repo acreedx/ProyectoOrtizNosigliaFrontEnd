629abe33bafb70c6fb1846422d516d15cfea96fe
Adrian Herrera <adrianhlinares@gmail.com>
2024-10-23 20:43:19 -0400

    cambios para móvil

--- src/app/api/createInitialUser.ts
+++ src/app/api/createInitialUser.ts
@@ -46,6 +46,41 @@ export default async function handler(
         },
       });
 
+      const dentistaRole = await prisma.rol.findUnique({
+        where: { roleName: "Dentista" },
+      });
+
+      if (!dentistaRole) {
+        return res
+          .status(404)
+          .json({ error: "Rol de Administrador no encontrado" });
+      }
+
+      const initialDenstist = await prisma.person.create({
+        data: {
+          active: true,
+          firstName: "Jaime",
+          secondName: "Adrian",
+          familyName: "Herrera",
+          gender: "Masculino",
+          birthDate: new Date("2003-06-05"),
+          email: "adrianhlinares@gmail.com",
+          addressLine: "Calle Francisco Katari #1338",
+          addressCity: "La Paz",
+          maritalStatus: "Single",
+          identification: "13679995",
+          phone: "2285515",
+          mobile: "73744202",
+          username: "DentistaInicial",
+          password: await hashPassword("1234Aa@"),
+          photoUrl: "https://example.com/photo.jpg",
+          passwordExpiration: getPasswordExpiration(),
+          rol: {
+            connect: { id: administradorRole.id },
+          },
+        },
+      });
+
       res.status(200).json({
         message: "Usuario Administrador creado correctamente",
         initialUser,
